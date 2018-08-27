604bb09070940c5c3f7fd9694481d7b22abf1be1
Seth MacPherson <smacpherson64@gmail.com>
2018-08-27 08:09:28 -0500

    fix: Added more tests

--- src/__tests__/get-ancestors.js
+++ src/__tests__/get-ancestors.js
@@ -2,8 +2,18 @@ import { isElement } from './utils/is-element';
 import { getAncestors } from '../get-ancestors';
 
 describe('getAncestors', () => {
+  test('returns an array', () => {
+    const results = getAncestors(document.documentElement);
+    expect(Array.isArray(results)).toBe(true);
+  });
+
   test('returns an array of elements', () => {
     const results = getAncestors(document.documentElement);
     expect(results.filter(isElement).length).toBe(1);
+  });
+
+  test('returns the correct element', () => {
+    const results = getAncestors(document.documentElement);
+    expect(results[0]).toBe(document.documentElement);
   });
 });
